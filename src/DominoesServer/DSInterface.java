2e5c2ba1b6a6c2854b4b98189820a8942cabfb38
pioavenger <sergio.aguiar@ua.pt>
2020-10-17 18:30:20 +0100

    Interface for new functions

--- src/DominoesServer/DSInterface.java
+++ src/DominoesServer/DSInterface.java
@@ -29,8 +29,13 @@ public class DSInterface
                 break;
             case 2:
             case 4:
+            case 5:
                 break;
             case 3:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
                 if (inMessage.noFirstArgument())
                     throw new DMessageException("Argument \"tableID\" was not given.", inMessage);
                 if ((int) inMessage.getFirstArgument() < 0)
@@ -60,8 +65,31 @@ public class DSInterface
                 int return4 = this.dsImplementation.joinRandomTable(inMessage.getPseudonym());
                 outMessage = new DMessage(DMessage.MessageType.JOIN_RANDOM_TABLE_REQUEST.getMessageCode(), return4);
                 break;
+            case 5:
+                DominoesTable return5 = this.dsImplementation.listTableInfo(inMessage.getPseudonym(),
+                        (int) inMessage.getFirstArgument());
+                outMessage = new DMessage(DMessage.MessageType.LIST_TABLE_INFO_REQUEST.getMessageCode(), return5);
+                break;
+            case 6:
+                this.dsImplementation.disbandTable(inMessage.getPseudonym(), (int) inMessage.getFirstArgument());
+                outMessage = new DMessage(DMessage.MessageType.DISBAND_TABLE_REQUEST.getMessageCode(), (Object) null);
+                break;
+            case 7:
+                boolean return7 = this.dsImplementation.leaveTable(inMessage.getPseudonym(),
+                        (int) inMessage.getFirstArgument());
+                outMessage = new DMessage(DMessage.MessageType.LEAVE_TABLE_REQUEST.getMessageCode(), return7);
+                break;
+            case 8:
+                boolean return8 = this.dsImplementation.startGame(inMessage.getPseudonym(),
+                        (int) inMessage.getFirstArgument());
+                outMessage = new DMessage(DMessage.MessageType.START_GAME_REQUEST.getMessageCode(), return8);
+                break;
+            case 9:
+                boolean return9 = this.dsImplementation.markAsReady(inMessage.getPseudonym(),
+                        (int) inMessage.getFirstArgument());
+                outMessage = new DMessage(DMessage.MessageType.MARK_AS_READY_REQUEST.getMessageCode(), return9);
+                break;
         }
-
         return outMessage;
     }
 }
